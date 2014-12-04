ff506ef6870e6e66a7d34a77f03fbc809fd85534
sberry <simon_berry2003@yahoo.com>
2014-12-04 22:32:41 +1030

    Tidied up column mapping in account table

--- src/main/java/com/hillsballetschool/pages/account/AccountPage.java
+++ src/main/java/com/hillsballetschool/pages/account/AccountPage.java
@@ -6,10 +6,8 @@ import javax.inject.Inject;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.StringResourceModel;
 
 import com.google.common.collect.ImmutableList;
 import com.hillsballetschool.domain.Account;
@@ -28,14 +26,14 @@ public class AccountPage extends MenuWebPage {
 	
 	public AccountPage() {
         List<? extends IColumn<Account, String>> columns = ImmutableList.of(
-           	new PropertyColumn<Account, String>(new Model<String>("Number"), Account.Fields.ID, Account.Fields.ID),
-        	new PropertyColumn<Account, String>(new StringResourceModel("givenNameTableHeaderLabel", this, null), Account.Fields.GIVEN_NAME, Account.Fields.GIVEN_NAME),
-        	new PropertyColumn<Account, String>(new Model<String>("Surname"), Account.Fields.SURNAME, Account.Fields.SURNAME),
-        	new PropertyColumn<Account, String>(new Model<String>("Email Address"), Account.Fields.EMAIL_ADDRESS, Account.Fields.EMAIL_ADDRESS),
-        	new PropertyColumn<Account, String>(new Model<String>("Phone"), Account.Fields.PHONE, Account.Fields.PHONE),
-        	new PropertyColumn<Account, String>(new Model<String>("Address1"), Account.Fields.ADDRESS1, Account.Fields.ADDRESS1),
-        	new PropertyColumn<Account, String>(new Model<String>("Address2"), Account.Fields.ADDRESS2, Account.Fields.ADDRESS2),
-        	new PropertyColumn<Account, String>(new Model<String>("Postcode"), Account.Fields.POSTCODE, Account.Fields.POSTCODE),
+        	new ResourcePropertyColumn<Account>(Account.Fields.ID, this),
+        	new ResourcePropertyColumn<Account>(Account.Fields.GIVEN_NAME, this),
+        	new ResourcePropertyColumn<Account>(Account.Fields.SURNAME, this),
+        	new ResourcePropertyColumn<Account>(Account.Fields.EMAIL_ADDRESS, this),
+        	new ResourcePropertyColumn<Account>(Account.Fields.PHONE, this),
+        	new ResourcePropertyColumn<Account>(Account.Fields.ADDRESS1, this),
+        	new ResourcePropertyColumn<Account>(Account.Fields.ADDRESS2, this),
+        	new ResourcePropertyColumn<Account>(Account.Fields.POSTCODE, this),
         	new LinkColumn<Account>(new Model<String>(""), Account.Fields.ID, "Edit")
         );
 		add(new BookmarkablePageLink<Void>("createAccountLink", EditAccountPage.class));
---
+++ src/main/java/com/hillsballetschool/pages/account/ResourcePropertyColumn.java
@@ -0,0 +1,17 @@
+package com.hillsballetschool.pages.account;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
+import org.apache.wicket.model.StringResourceModel;
+
+/**
+ * Wraps a {@link PropertyColumn} that gets the label from a property file using {@link StringResourceModel}
+ * 
+ * @param <T>
+ */
+@SuppressWarnings("serial")
+public class ResourcePropertyColumn<T> extends PropertyColumn<T, String> {
+	public ResourcePropertyColumn(String id, Component component) {
+		super(new StringResourceModel(id, component, null), id, id);
+	}
+}
