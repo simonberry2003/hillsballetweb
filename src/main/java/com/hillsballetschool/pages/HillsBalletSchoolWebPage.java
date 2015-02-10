89b6d86d75bb005a50590a2a578d1fa778cfec9e
sberry <simon_berry2003@yahoo.com>
2015-02-10 22:44:28 +1030

    Basic css for tables

--- src/main/java/com/hillsballetschool/pages/HillsBalletSchoolWebPage.java
+++ src/main/java/com/hillsballetschool/pages/HillsBalletSchoolWebPage.java
@@ -1,8 +1,11 @@
 package com.hillsballetschool.pages;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.CssResourceReference;
 
 /**
  * Base {@link WebPage} class for all web pages. Has the addition of some useful methods.
@@ -25,5 +28,10 @@ public abstract class HillsBalletSchoolWebPage extends WebPage implements Parame
 	@Override
 	public String getParameter(String parameterName) {
 		return getPageParameters().get(parameterName).toString();
+	}
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		response.render(CssHeaderItem.forReference(new CssResourceReference(HillsBalletSchoolWebPage.class, "HillsBalletSchool.css")));
 	}
 }
